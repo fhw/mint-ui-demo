43d90652546123b524e43588ad6922865f7ef11a
fhw <fhw5629@gmail.com>
2017-05-18 13:56:22 +0800

    引入饿了么组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <topnav></topnav>
+        <img src="./assets/logo.png">
+        <router-view></router-view>
+    </div>
 </template>
-
 <script>
+import topnav from './components/topnav.vue'
 export default {
-  name: 'app'
+    name: 'app',
+    components: {
+        topnav
+    }
 }
 </script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 60px;*/
 }
 </style>
--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -1,27 +1,32 @@
 <template>
-	<div id="top-nav" class="nav top-nav">
-		<div class="nav-d1">
-			<span>1</span>
-		</div>
-		<div class="nav-d2">
-			<span>2</span>
-		</div>
-	</div>
+    <div id="topnav">
+        <el-col :xs="24">
+            <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">首页</el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">列表页</template>
+                    <el-menu-item index="2-1">选项1</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                    <el-menu-item index="2-3">选项3</el-menu-item>
+                </el-submenu>
+                <el-menu-item index="3">详情页</el-menu-item>
+            </el-menu>
+        </el-col>
+    </div>
 </template>
-<script type="text/javascript">
-	export default{
-		name:"top-nav"
-	}
+<script>
+export default {
+    name: 'topnav',
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+}
 </script>
-<style type="text/css">
-	.nav{
-		height: 50px;
-		width: 100%;
-		line-height: 50px;
-		background: yellow;
-	}
-	.top-nav{
-		position: fixed;
-		top: 0;
-	}
-</style>
