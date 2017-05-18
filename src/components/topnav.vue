c69e8fce2c2bbe880417caaa2bc55dda65f37169
fhw <fhw5629@gmail.com>
2017-05-18 14:38:59 +0800

    饿了么组件熟悉中

--- src/components/topnav.vue
+++ src/components/topnav.vue
@@ -1,17 +1,22 @@
 <template>
     <div id="topnav">
-        <el-col :xs="24">
-            <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-                <el-menu-item index="1">首页</el-menu-item>
-                <el-submenu index="2">
-                    <template slot="title">列表页</template>
-                    <el-menu-item index="2-1">选项1</el-menu-item>
-                    <el-menu-item index="2-2">选项2</el-menu-item>
-                    <el-menu-item index="2-3">选项3</el-menu-item>
-                </el-submenu>
-                <el-menu-item index="3">详情页</el-menu-item>
-            </el-menu>
-        </el-col>
+        <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-row type="flex" justify="center">
+                <el-col :span="2">
+                    <i class="el-icon-picture"></i>
+                </el-col>
+                <el-col :span="10">
+                    <el-menu-item index="1">首页</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">列表页</template>
+                        <el-menu-item index="2-1">选项1</el-menu-item>
+                        <el-menu-item index="2-2">选项2</el-menu-item>
+                        <el-menu-item index="2-3">选项3</el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="3">详情页</el-menu-item>
+                </el-col>
+            </el-row>
+        </el-menu>
     </div>
 </template>
 <script>
