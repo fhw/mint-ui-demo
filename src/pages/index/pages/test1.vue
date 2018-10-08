bb8c655f138a5ac19d2138500f4052fe1ace716d
fenghaowei <fenghaowei@hzed.com>
2018-10-08 10:50:28 +0800

    test multi page route,work normal

---
+++ src/pages/index/pages/test1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        test1
+        <router-link to="/test2">test2</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+  created () {
+    console.log('222')
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
